82561678f73949800f3b181a33a765aa74072847
SD <skyyblucloud@gmail.com>
2019-11-04 06:02:08 +0900

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,107 +1,105 @@
 #!/bin/bash
-while true; do
-    echo "---Checking for 'runtime' folder---"
-    if [ ! -d ${SERVER_DIR}/runtime ]; then
-      echo "---'runtime' folder not found, creating...---"
-      mkdir ${SERVER_DIR}/runtime
-    else
-      echo "---"runtime" folder found---"
-    fi
+echo "---Checking for 'runtime' folder---"
+if [ ! -d ${SERVER_DIR}/runtime ]; then
+	echo "---'runtime' folder not found, creating...---"
+	mkdir ${SERVER_DIR}/runtime
+else
+	echo "---"runtime" folder found---"
+fi
 
-    echo "---Checking if Runtime is installed---"
-    if [ -z "$(find ${SERVER_DIR} -name jre*)" ]; then
-        if [ "${RUNTIME_NAME}" == "jre1.8.0_211" ]; then
-          echo "---Downloading and installing Runtime---"
-        cd ${SERVER_DIR}/runtime
-        wget -qi ${RUNTIME_NAME} https://github.com/ich777/docker-minecraft-basic-server/raw/master/runtime/8u211.tar.gz
-            tar --directory ${SERVER_DIR}/runtime -xvzf ${SERVER_DIR}/runtime/8u211.tar.gz
-            rm -R ${SERVER_DIR}/runtime/8u211.tar.gz
-        else
-          if [ ! -d ${SERVER_DIR}/runtime/${RUNTIME_NAME} ]; then
-              echo "---------------------------------------------------------------------------------------------"
-              echo "---Runtime not found in folder 'runtime' please check again! Putting server in sleep mode!---"
-              echo "---------------------------------------------------------------------------------------------"
-              sleep infinity
-            fi
-        fi
+echo "---Checking if Runtime is installed---"
+if [ -z "$(find ${SERVER_DIR} -name jre*)" ]; then
+    if [ "${RUNTIME_NAME}" == "jre1.8.0_211" ]; then
+    	echo "---Downloading and installing Runtime---"
+		cd ${SERVER_DIR}/runtime
+		wget -qi ${RUNTIME_NAME} https://github.com/ich777/docker-minecraft-basic-server/raw/master/runtime/8u211.tar.gz
+        tar --directory ${SERVER_DIR}/runtime -xvzf ${SERVER_DIR}/runtime/8u211.tar.gz
+        rm -R ${SERVER_DIR}/runtime/8u211.tar.gz
     else
-      echo "---Runtime found---"
-    fi      
-
-    echo "---Checking for Minecraft Server executable ---"
-    #if [ ! -f $SERVER_DIR/${JAR_NAME}.jar ]; then
-      cd ${SERVER_DIR}
-      echo "---Downloading Minecraft Server 1.14.3---"
-        wget -qi ${JAR_NAME} https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
-        sleep 2
-        if [ ! -f $SERVER_DIR/${JAR_NAME}.jar ]; then
-          echo "----------------------------------------------------------------------------------------------------"
-          echo "---Something went wrong, please install Minecraft Server manually. Putting server into sleep mode---"
-            echo "----------------------------------------------------------------------------------------------------"
-            sleep infinity
-        fi
-    #else
-      #echo "---Minecraft Server executable found---"
-    #fi
-
-    echo "---Preparing Server---"
-    echo "---Checking for 'server.properties'---"
-    if [ ! -f ${SERVER_DIR}/server.properties ]; then
-        echo "---No 'server.properties' found, downloading...---"
-        wget -qi ${SERVER_DIR}/server.properties https://raw.githubusercontent.com/ich777/docker-minecraft-basic-server/master/config/server.properties
-    else
-        echo "---'server.properties' found..."
-    fi
-    chmod -R 770 ${DATA_DIR}
-    if [ ! -f $SERVER_DIR/eula.txt ]; then
-      :
-    else
-      if [ "${ACCEPT_EULA}" == "false" ]; then
-        if grep -rq 'eula=true' ${SERVER_DIR}/eula.txt; then
-          sed -i '/eula=true/c\eula=false' ${SERVER_DIR}/eula.txt
-        fi
-        echo
-        echo "-------------------------------------------------------"
-          echo "------EULA not accepted, you must accept the EULA------"
-          echo "---to start the Server, putting server in sleep mode---"
-          echo "-------------------------------------------------------"
-          sleep infinity
+    	if [ ! -d ${SERVER_DIR}/runtime/${RUNTIME_NAME} ]; then
+        	echo "---------------------------------------------------------------------------------------------"
+        	echo "---Runtime not found in folder 'runtime' please check again! Putting server in sleep mode!---"
+        	echo "---------------------------------------------------------------------------------------------"
+        	sleep infinity
         fi
     fi
-    echo "---Checking for old logs---"
-    find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
+else
+	echo "---Runtime found---"
+fi      
 
-    echo "---Starting Server---"
-    cd ${SERVER_DIR}
-    screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/runtime/${RUNTIME_NAME}/bin/java -Xmx${XMX_SIZE}M -Xms${XMS_SIZE}M -jar ${SERVER_DIR}/${JAR_NAME}.jar nogui ${GAME_PARAMS}
+echo "---Checking for Minecraft Server executable ---"
+#if [ ! -f $SERVER_DIR/${JAR_NAME}.jar ]; then
+	cd ${SERVER_DIR}
+	echo "---Downloading Minecraft Server 1.14.3---"
+    wget -qi ${JAR_NAME} https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
     sleep 2
-    if [ ! -f $SERVER_DIR/eula.txt ]; then
-      echo "---EULA not found please stand by...---"
-      sleep 30
-    fi
-    if [ "${ACCEPT_EULA}" == "true" ]; then
-      if grep -rq 'eula=false' ${SERVER_DIR}/eula.txt; then
-          sed -i '/eula=false/c\eula=true' ${SERVER_DIR}/eula.txt
-        echo "---EULA accepted, please restart server---"
-            sleep infinity
-        fi
-    elif [ "${ACCEPT_EULA}" == "false" ]; then
-      echo
-      echo "-------------------------------------------------------"
-        echo "------EULA not accepted, you must accept the EULA------"
-        echo "---to start the Server, putting server in sleep mode---"
-        echo "-------------------------------------------------------"
+    if [ ! -f $SERVER_DIR/${JAR_NAME}.jar ]; then
+    	echo "----------------------------------------------------------------------------------------------------"
+    	echo "---Something went wrong, please install Minecraft Server manually. Putting server into sleep mode---"
+        echo "----------------------------------------------------------------------------------------------------"
         sleep infinity
-    else
-      echo "---Something went wrong, please check EULA variable---"
     fi
-    echo "---Waiting for logs, please stand by...---"
-    sleep 30
-    if [ -f ${SERVER_DIR}/logs/latest.log ]; then
-            tail -F ${SERVER_DIR}/logs/latest.log
-    else
-            tail -f ${SERVER_DIR}/masterLog.0
+#else
+	#echo "---Minecraft Server executable found---"
+#fi
+
+echo "---Preparing Server---"
+echo "---Checking for 'server.properties'---"
+if [ ! -f ${SERVER_DIR}/server.properties ]; then
+    echo "---No 'server.properties' found, downloading...---"
+    wget -qi ${SERVER_DIR}/server.properties https://raw.githubusercontent.com/ich777/docker-minecraft-basic-server/master/config/server.properties
+else
+    echo "---'server.properties' found..."
+fi
+chmod -R 770 ${DATA_DIR}
+if [ ! -f $SERVER_DIR/eula.txt ]; then
+	:
+else
+	if [ "${ACCEPT_EULA}" == "false" ]; then
+		if grep -rq 'eula=true' ${SERVER_DIR}/eula.txt; then
+			sed -i '/eula=true/c\eula=false' ${SERVER_DIR}/eula.txt
+		fi
+		echo
+		echo "-------------------------------------------------------"
+    	echo "------EULA not accepted, you must accept the EULA------"
+    	echo "---to start the Server, putting server in sleep mode---"
+    	echo "-------------------------------------------------------"
+    	sleep infinity
     fi
-    echo "RESTARTING IN 10 SECONDS"
-    sleep 10000
-done
+fi
+echo "---Checking for old logs---"
+find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
+
+echo "---Starting Server---"
+cd ${SERVER_DIR}
+#screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/runtime/${RUNTIME_NAME}/bin/java -Xmx${XMX_SIZE}M -Xms${XMS_SIZE}M -jar ${SERVER_DIR}/${JAR_NAME}.jar nogui ${GAME_PARAMS}
+screen -S Minecraft -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m  /opt/scripts/serverloop.sh
+
+#sleep 2
+#if [ ! -f $SERVER_DIR/eula.txt ]; then
+#	echo "---EULA not found please stand by...---"
+#	sleep 30
+#fi
+#if [ "${ACCEPT_EULA}" == "true" ]; then
+#	if grep -rq 'eula=false' ${SERVER_DIR}/eula.txt; then
+#    	sed -i '/eula=false/c\eula=true' ${SERVER_DIR}/eula.txt
+#		echo "---EULA accepted, please restart server---"
+#        sleep infinity
+#    fi
+#elif [ "${ACCEPT_EULA}" == "false" ]; then
+#	echo
+#	echo "-------------------------------------------------------"
+#    echo "------EULA not accepted, you must accept the EULA------"
+#    echo "---to start the Server, putting server in sleep mode---"
+#    echo "-------------------------------------------------------"
+#    sleep infinity
+#else
+#	echo "---Something went wrong, please check EULA variable---"
+#fi
+#echo "---Waiting for logs, please stand by...---"
+#sleep 30
+#if [ -f ${SERVER_DIR}/logs/latest.log ]; then
+#        tail -F ${SERVER_DIR}/logs/latest.log
+#else
+#        tail -f ${SERVER_DIR}/masterLog.0
+#fi
