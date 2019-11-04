43b07b22ecf146b4980f555a54926f31e6d0de1e
SD <skyyblucloud@gmail.com>
2019-11-04 16:23:43 +0900

    Update serverloop.sh

--- scripts/serverloop.sh
+++ scripts/serverloop.sh
@@ -1,5 +1,6 @@
-while true; do
+while : 
+do
     ${SERVER_DIR}/runtime/${RUNTIME_NAME}/bin/java -Xmx${XMX_SIZE}M -Xms${XMS_SIZE}M -jar ${SERVER_DIR}/${JAR_NAME}.jar nogui ${GAME_PARAMS}
     echo "RESTARTING IN 10 SECONDS"
-    sleep 10000
+    sleep 10
 done
